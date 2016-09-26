7a6d984bc40fe383c5d0b9940eea780bf69fd291
Paul Savignano <Paul.Savignano@gmail.com>
2016-09-26 15:24:03 -0700

    Add html-pdf, react-inline-css, file-saver.

---
+++ imports/api/documents/documents.js
@@ -0,0 +1,31 @@
+import faker from 'faker'
+import { Mongo } from 'meteor/mongo'
+import { SimpleSchema } from 'meteor/aldeed:simple-schema'
+import { Factory } from 'meteor/dburles:factory'
+
+export const Documents = new Mongo.Collection('Documents')
+
+Documents.allow({
+  insert: () => false,
+  update: () => false,
+  remove: () => false,
+})
+
+Documents.deny({
+  insert: () => true,
+  update: () => true,
+  remove: () => true,
+})
+
+Documents.schema = new SimpleSchema({
+  title: {
+    type: String,
+    label: 'The title of the document.',
+  },
+})
+
+Documents.attachSchema(Documents.schema)
+
+Factory.define('document', Documents, {
+  title: () => faker.hacker.phrase(),
+})
